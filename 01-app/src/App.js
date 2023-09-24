17a5880bbb125103a382bea964dfe93433fb333f
haodada <jflachenjiahao@163.com>
2023-09-24 19:58:47 +0800

    react

--- 01-app/src/App.js
+++ 01-app/src/App.js
@@ -1,32 +1,13 @@
 import React, {Component} from 'react'
-import {Route, Switch, Redirect} from 'react-router-dom'
 import './App.scss'
-import About from './pages/about/about'
-import Home from './pages/home/home'
-import Header from './components/header/header'
-import MyNavLink from './components/myNavLink/myNavLink'
+import {Button} from 'antd'
+import 'antd/dist/reset.css'
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <Header></Header>
-        {/* 原生html 靠<a>跳转到不同页面 */}
-        {/* react中靠路由链接切换组件 -- 编写 */}
-        {/* <NavLink activeClassName="atguigu" to="/about">About</NavLink> */}
-        {/* <NavLink activeClassName="atguigu" to="/home">Home</NavLink> */}
-        {/*                     直接传入props key为children */}
-        <MyNavLink to="/about" >About</MyNavLink>
-        <MyNavLink to="/home" >Home</MyNavLink>
-
-        {/* 注册路由 */}
-        {/*                   路由组件 会收到路由器传递的参数 */}
-        {/* 包裹了switch 如果已经匹配上了路由就不会再往下匹配 */}
-        <Switch> 
-          <Route path="/about" component={About}></Route>
-          <Route path="/home" component={Home}></Route>
-          <Redirect to="/about"></Redirect>
-        </Switch>
-  
+        app
+        <Button type="primary">Primary Button</Button>
       </div>
     )
   }
--- 01-app/src/index.js
+++ 01-app/src/index.js
@@ -1,29 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* history模式的组件 */}
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </React.StrictMode>
 );
-
-// BrowserRouter与HashRouter的区别
-// 1、底层原理不一样
-  // BrowserRouter使用的是history API
-  // HashRouter使用的是URL的哈希值
-// 2、path表现形式不一样
-  // BrowserRouter的路径中没有#, 例如 localhost：3000/demo/test
-  // HashRouter的路径包含#，例如 localhost：3000/#demo/test
-// 3、刷新后对路由state参数的影响
-  // (1).BrowserRouter没有任何影响 因为state保存在history对象中
-  // (2).HashRouter刷新后会导致路由state参数的丢失
-// 4、备注：HashRouter可以用于解决一些路径错误相关的问题
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
